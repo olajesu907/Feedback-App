0f2f38599f9dac4f532fb8070e68081a9236ee24
GodWithUs <emmanuel.olajesu@cset.uniosun.edu.ng>
2023-09-01 19:28:19 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App(){
+    return (
+    <div className="container">
+        <h1>Hello from the app component</h1>
+        <p>Hello</p>
+        
+    </div>
+    )
+}
+export default App
